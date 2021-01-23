d8be77a97a0df71dc515ed4f1ba62f1d043b3bf2
Lokithemanic <32976974+Lokithemanic@users.noreply.github.com>
2021-01-23 15:58:42 +0530

    Update pmpermit.py

--- fridaybot/modules/pmpermit.py
+++ fridaybot/modules/pmpermit.py
@@ -12,7 +12,7 @@ from fridaybot.utils import friday_on_cmd
 
 PMPERMIT_PIC = os.environ.get("PMPERMIT_PIC", None)
 if PMPERMIT_PIC is None:
-    WARN_PIC = "https://telegra.ph/file/53aed76a90e38779161b1.jpg"
+    WARN_PIC = "https://telegra.ph/file/b3b0e08c6e4e3339a9ee3.jpg"
 else:
     WARN_PIC = PMPERMIT_PIC
 
@@ -25,7 +25,7 @@ DEFAULTUSER = (
     str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
 )
 CUSTOM_MIDDLE_PMP = (
-    str(CUSTOM_PMPERMIT) if CUSTOM_PMPERMIT else "Protection By Friday 🇮🇳"
+    str(CUSTOM_PMPERMIT) if CUSTOM_PMPERMIT else "bhakk btc"
 )
 USER_BOT_WARN_ZERO = "You Have Attempted To Spam Masters Inbox So Inorder To Avoid Over Spam , You Have Been Blocked By Userbot"
 
@@ -34,8 +34,8 @@ botisnoob = Var.TG_BOT_USER_NAME_BF_HER
 devs_id = [1263617196, 573738900, 1315076555]
 
 USER_BOT_NO_WARN = (
-    "**Hello, This is Friday PM Protection Service ⚠️**\n\n"
-    f"`My Master {DEFAULTUSER} is Busy Right Now !` \n"
+    "**Hello, This is My PM Protection Service ⚠️**\n\n"
+    f"`{DEFAULTUSER} is Busy Right Now !` \n"
     "**I Request You To Choose A Reason You Have Came For** 👀 \n\n"
     f"**{CUSTOM_MIDDLE_PMP}**"
 )
